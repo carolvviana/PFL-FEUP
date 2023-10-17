d9a7ba0a502bb8d27d00daf0dfe800e10d57504e
Sergio Peixoto <sergiopeixoto3@gmail.com>
2023-10-17 12:19:43 +0100

    board drawing dynamic now

--- draw.pl
+++ draw.pl
@@ -1,12 +1,28 @@
 %print the matrix of the game
-p_m([]):- nl, write('--+-+--').
-p_m([L|T]):-
+repeat_string(_, 0, '').
+repeat_string(Str, N, Result):-
+    N > 0,
+    NewN is N - 1,
+    repeat_string(Str, NewN, NewResult),
+    atom_concat(Str, NewResult, Result).
+
+print_separator(N):-
+    repeat_string('-+', N-1, Result),
+    atom_concat('+', Result, TempResult),
+    atom_concat(TempResult, '-+', FinalResult),
+    write(FinalResult), !.
+
+p_m([], N):-
     nl,
-    write('--+-+--'),
+    print_separator(N).
+
+p_m([L|T], N):-
+    nl,
+    print_separator(N),
     nl,
     p_l(L), 
     write('|'),
-    p_m(T).
+    p_m(T, N).
 
 %prints a line of the matrix
 %TODO: change to have a better representation
@@ -15,39 +31,12 @@ p_l([C|L]):-
     write('|'), p_c(C),
     p_l(L).
 
-%prints a character of the matrix
-% p_c(_-_-_-_-_-_-X):-
-%     write(X), !.
-
-% p_c(_-_-_-_-_-X):-
-%     write(X), !.
-
-% p_c(_-_-_-_-X):-
-%     write(X), !.
-
-% p_c(_-_-_-X):-
-%     write(X), !.
-
-% p_c(_-_-X):-
-%     write(X), !.
-
 p_c(_-X):-
     write(X), !.
 
 p_c(empty):-
     write(' '), !.
 
-_ -> 1-1-a-X
-
-%     t(C,S),
-%     wi
-
-% p_c(C):-
-%     t(C,S),
-%     write(S).
-
-% t():-
-
-
-% 1-A-2
-% p_c()
+% |2|1|
+% --+--
+% 
