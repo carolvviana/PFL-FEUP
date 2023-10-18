461fcd2b1a29cdd332cf3c37d14bd7600fd8c2a4
carol viana <carolinacoutoviana@gmail.com>
2023-10-18 16:23:42 +0100

    knight valid moves working

--- game.pl
+++ game.pl
@@ -26,6 +26,7 @@ replace_row(Row, X, Piece, NewRow) :-
     nth0(X, Row, _, TempRow),
     nth0(X, NewRow, Piece, TempRow).
 
+
 %_______________________________________________________________________________________
 
 
@@ -54,12 +55,13 @@ valid_pawn_coords(Board, X1, Y1, X2, Y2) :-
     Y2 = Y1,
     get_piece(Board, X2, Y2, Piece),
     Piece \= empty.
+
 %_______________________________________________________________________________________
 
 % get all possible coords for a rook, where not empty
 % moves any number of squares orthogonally on the first tower in its path.
 % valid_rook_coords(+Board, +X1, +Y1, +Direction)
-valid_rook_coords(Board, X1, Y1, X2, Y2, Dir):-
+/* valid_rook_coords(Board, X1, Y1, X2, Y2, Dir):-
     % get Y size of board
     length(Board, YSize),
     % get X size of board
@@ -70,7 +72,7 @@ valid_rook_coords(Board, X1, Y1, X2, Y2, Dir):-
     X2 >= 0,
     X2 < XSize,
     Y2 >= 0,
-    Y2 < YSize,
+    Y2 < YSize.
     %TODO:
 
 valid_rook_coords(Board, X1, Y1, X2, Y2, down) :-
@@ -99,8 +101,66 @@ valid_rook_coords(Board, X1, Y1, X2, Y2, right) :-
     Y2 = Y1,
     valid_rook_coords(Board, X2, Y1, X2, Y2, right).
     get_piece(Board, X2, Y2, Piece),
+    Piece \= empty. */
+
+%_____________________________________________________________
+
+% get all possible coords for a knight, where not empty
+% valid_knight_coords(+Board, +X1, +Y1, ?X2, ?Y2)
+valid_knight_coords(Board, X1, Y1, X2, Y2) :-
+    X2 is (X1 + 1),
+    Y2 is (Y1 + 2),
+    get_piece(Board, X2, Y2, Piece),
     Piece \= empty.
 
+valid_knight_coords(Board, X1, Y1, X2, Y2) :-
+    X2 is (X1 + 2),
+    Y2 is (Y1 + 1),
+    get_piece(Board, X2, Y2, Piece),
+    Piece \= empty.
+
+valid_knight_coords(Board, X1, Y1, X2, Y2) :-    
+    X2 is (X1 + 1),
+    Y2 is (Y1 - 2),
+    get_piece(Board, X2, Y2, Piece),
+    Piece \= empty.  
+
+valid_knight_coords(Board, X1, Y1, X2, Y2) :-
+    X2 is (X1 - 1),
+    Y2 is (Y1 + 2),
+    get_piece(Board, X2, Y2, Piece),
+    Piece \= empty.
+
+valid_knight_coords(Board, X1, Y1, X2, Y2) :-
+    X2 is (X1 + 2),
+    Y2 is (Y1 - 1),
+    get_piece(Board, X2, Y2, Piece),
+    Piece \= empty.
+
+valid_knight_coords(Board, X1, Y1, X2, Y2) :-
+    X2 is (X1 - 1),
+    Y2 is (Y1 - 2),
+    get_piece(Board, X2, Y2, Piece),
+    Piece \= empty.
+
+valid_knight_coords(Board, X1, Y1, X2, Y2) :-
+    X2 is (X1 - 2),
+    Y2 is (Y1 + 1),
+    get_piece(Board, X2, Y2, Piece),
+    Piece \= empty.
+
+valid_knight_coords(Board, X1, Y1, X2, Y2) :-
+    X2 is (X1 - 2),
+    Y2 is (Y1 - 1),
+    get_piece(Board, X2, Y2, Piece),
+    Piece \= empty.
+
+%______________________________________________________________________________
+
+% get all possible coords for a knight, where not empty
+% knight_coords(+Board, +X1, +Y1, -Result)
+knight_coords(Board, X1, Y1, Result) :-
+    findall(X2-Y2, valid_knight_coords(Board, X1, Y1, X2, Y2), Result).
 
 % get all possible coords for a rook, where not empty
 % rook_coords(+Board, +X1, +Y1, -Result)
@@ -111,6 +171,8 @@ rook_coords(Board, X1, Y1, Result) :-
 % pawn_coords(+Board, +X1, +Y1, -Result)
 pawn_coords(Board, X1, Y1, Result) :-
     findall(X2-Y2, valid_pawn_coords(Board, X1, Y1, X2, Y2), Result).
+
+%______________________________________________________________________________
 
 % move piece of size N
 % move_piece(+Board, +X1, +Y1, +X2, +Y2, +N, -NewBoard)
@@ -128,6 +190,9 @@ move_piece(Board, X1, Y1, X2, Y2, 4, NewBoard):-
 
 move_piece(Board, X1, Y1, X2, Y2, 5, NewBoard):-
     move_queen(Board, X1, Y1, X2, Y2, NewBoard).
+
+
+%______________________________________________________________________________
 
 
 % move pawn to a given position
